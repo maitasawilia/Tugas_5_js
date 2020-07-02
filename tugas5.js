5e6000219ad2e964205acb4276a5a2efa21fc306
maitasawilia <maitasaw@gmail.com>
2020-07-02 22:00:52 +0700

    Tugas 5 JS
    Tugas ini kami kumpulkan untuk Niomic

--- tugas5.js
+++ tugas5.js
@@ -1,27 +1,11 @@
-for(var i=3;i<=21;i++){
-  if (i==4)
-  continue;
-  if (i==5)
-  continue;
-  if (i==7)
-  continue;
-  if (i==8)
-  continue;
-  if (i==10)
-  continue;
-  if (i==11)
-  continue;
-  if (i==13)
-  continue;
-  if (i==14)
-  continue;
-  if (i==16)
-  continue;
-  if (i==17)
-  continue;
-  if (i==19)
-  continue;
-  if (i==20)
-  continue;
-  console.log(i)
+function panggilKelipatan(){
+  var kelipatan = []
+  for (var i = 1; i <= 20; i++) {
+    if(i%3 == 0){
+      kelipatan.push(i)
+    }
+  }
+  return kelipatan;
 }
+
+console.log(panggilKelipatan());
